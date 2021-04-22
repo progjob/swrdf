a83fd8b2e59944e2c7bb34569f1289cf14ae1690
Dmitry Evstratov <progjob@mail.ru>
2021-04-22 22:09:02 +0300

    Add module for NSUserDefaults.class

--- hooks/fileManager.js
+++ hooks/fileManager.js
@@ -4,3 +4,11 @@ Interceptor.attach(ObjC.classes.NSFileManager['- fileExistsAtPath:'].implementat
         console.log('fileExistsAtPath:', '\n\t', ObjC.Object(args[2]).toString());
 	}
 });
+
+
+Interceptor.attach(ObjC.classes.NSFileManager['- createFileAtPath:contents:attributes:'].implementation, {
+    onEnter: function (args) {
+        console.log('createFileAtPath:contents:attributes:', '\n\t', ObjC.Object(args[2]).toString());
+	}
+});
+
---
+++ hooks/userDefaults.js
@@ -0,0 +1,15 @@
+
+var hookMethods = [
+	"- setObject:forKey:", "- setInteger:forKey:", "- setFloat:forKey:", "- setDouble:forKey:", "- setBool:forKey:", "- setURL:forKey:",
+	"- objectForKey:", "- integerForKey:", "- floatForKey:", "- doubleForKey:", "- boolForKey:", "- URLForKey:",
+	"- arrayForKey:", "- dictionaryForKey:", "- dataForKey:", "- stringForKey:", "- stringArrayForKey:",
+];
+
+hookMethods.forEach(function(m) {
+	var hook = ObjC.classes.NSUserDefaults[m];
+	Interceptor.attach(hook.implementation, {
+		onEnter: function(args) {
+	        console.log('UserDefaults', '\n\t', m);
+		}
+	});
+});
