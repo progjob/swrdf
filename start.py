dec8223cb776af62c64195098371d77a44500710
Dmitry Evstratov <progjob@mail.ru>
2021-04-23 14:53:27 +0300

    Fix modules bugs. Refactor launch script. Update readme.

--- start.py
+++ start.py
@@ -2,17 +2,27 @@
 import frida
 import sys
 import os 
+from optparse import OptionParser
 
 if __name__ == '__main__':
 
-	path = "./hooks"
-	session = frida.get_usb_device().attach("FridaTraceApp")
+	parser = OptionParser()
+	parser.add_option("-A", "--attach", default = False, action = "store_true")
 
-	for filename in os.listdir(path):
+	hooks_path = "./hooks"
+	app_to_attach = "FridaTraceApp"
+
+	(options, args) = parser.parse_args()
+	if (options.attach):
+		app_to_attach = str(args[0])
+
+	session = frida.get_usb_device().attach(app_to_attach)
+
+	for filename in os.listdir(hooks_path):
 		name, ext = os.path.splitext(filename)
 
 		if (ext == ".js"):
-			hook = open(path + os.sep + filename, "r")
+			hook = open(hooks_path + os.sep + filename, "r")
 			script = session.create_script(hook.read())
 			script.load()
 
